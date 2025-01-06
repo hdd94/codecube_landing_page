140a98b8123fc08db3dc24400d75660b9cd67e9c
Huy Duc Do <huy.duc.do@sap.com>
2025-01-06 18:49:49 +0100

    So

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -9,7 +9,6 @@ import { notFound } from 'next/navigation'
 
 type Props = {
     params: Promise<{ id: string }>
-    searchParams: { [key: string]: string | string[] | undefined }
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
